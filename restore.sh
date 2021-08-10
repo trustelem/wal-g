418e0db31a4bd0ae7217038ee6028b82e50099d1
Georges Varouchas <gvarouchas@wallix.com>
2021-08-10 19:12:08 +0200

    restore.sh: add time spent running wal-g backup-fetch
    as reported by the 'time' builtin in bash

--- restore.sh
+++ restore.sh
@@ -3,4 +3,9 @@
 rm -rf tmp/restored
 mkdir -p tmp
 
-./walg.sh backup-fetch tmp/restored LATEST
+TIMEFORMAT="elapsed: %Rs"
+time ./walg.sh backup-fetch tmp/restored LATEST
+
+cd tmp/restored
+
+find . -type f | sort | xargs md5sum > md5.restored
