64810898966698c2f4367da7ea35120909c8cf8e
Georges Varouchas <gvarouchas@wallix.com>
2021-08-11 10:53:39 +0200

    add zstd as compressor

--- internal/compression/zstd/compressor.go
+++ internal/compression/zstd/compressor.go
@@ -2,8 +2,10 @@ package zstd
 
 import (
 	"io"
+	"os"
 
 	"github.com/klauspost/compress/zstd"
+	"github.com/wal-g/tracelog"
 )
 
 const (
@@ -14,7 +16,30 @@ const (
 type Compressor struct{}
 
 func (compressor Compressor) NewWriter(writer io.Writer) io.WriteCloser {
-	zw, _ := zstd.NewWriter(writer, zstd.WithEncoderLevel(zstd.SpeedDefault))
+	speed := zstd.SpeedBestCompression
+
+	val, ok := os.LookupEnv("WALG_COMPRESSION_LEVEL")
+	if ok {
+		switch val {
+		case "fastest", "1":
+			speed = zstd.SpeedFastest
+		case "default", "fast", "2", "":
+			speed = zstd.SpeedDefault
+		case "better", "3":
+			speed = zstd.SpeedBetterCompression
+		case "best", "4":
+			speed = zstd.SpeedBestCompression
+
+		default:
+			tracelog.WarningLogger.Printf("zstd compressor: unknown compression level '%s', using 'default'", val)
+			tracelog.DebugLogger.Print("    possible values are : fastest|fast|better|best")
+		}
+	}
+
+	zw, err := zstd.NewWriter(writer, zstd.WithEncoderLevel(speed))
+	if err != nil {
+		panic(err)
+	}
 
 	return zw
 }
--- internal/compression/zstd/decompressor.go
+++ internal/compression/zstd/decompressor.go
@@ -13,9 +13,12 @@ type Decompressor struct{}
 
 func (decompressor Decompressor) Decompress(dst io.Writer, src io.Reader) error {
 	zstdReader, err := zstd.NewReader(computils.NewUntilEOFReader(src))
+	if err != nil {
+		return err
+	}
 	_, err = utility.FastCopy(dst, zstdReader)
 	if err != nil {
-		return errors.Wrap(err, "DecompressZstd: zstd write failed")
+		return errors.Wrap(err, "DecompressZstd: zstd copy failed")
 	}
 	zstdReader.Close()
 	return errors.Wrap(err, "DecompressZstd: zstd reader close failed")
